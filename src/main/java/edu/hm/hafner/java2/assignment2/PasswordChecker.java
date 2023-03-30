8d58a4ed3046d83d1e1fa890227f3fd16b6c5301
Ulli Hafner <ullrich.hafner@gmail.com>
2023-03-30 21:39:23 +0200

    Add assignment 2.

---
+++ src/main/java/edu/hm/hafner/java2/assignment2/PasswordChecker.java
@@ -0,0 +1,27 @@
+package edu.hm.hafner.java2.assignment2;
+
+/**
+ * Ensures that a password is strong enough.
+ */
+public class PasswordChecker {
+    /**
+     * Creates a new {@link PasswordChecker} instance.
+     *
+     * @param dictionary
+     *         a dictionary of not allowed passwords
+     */
+    public PasswordChecker(final String... dictionary) {
+    }
+
+    /**
+     * Returns whether the password is strong enough, or not.
+     *
+     * @param password
+     *         the password to check
+     *
+     * @return {@code true} if the password is strong enough, {@code false} otherwise
+     */
+    public boolean isValid(final String password) {
+        return true;
+    }
+}
---
+++ src/test/java/edu/hm/hafner/java2/assignment2/PasswordCheckerTest.java
@@ -0,0 +1,8 @@
+package edu.hm.hafner.java2.assignment2;
+
+/**
+ * Tests the class {@link PasswordChecker}.
+ */
+class PasswordCheckerTest {
+    // Hier kommen Ihre Tests rein
+}
